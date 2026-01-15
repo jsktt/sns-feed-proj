8922ef2880c70543e2e76c91b18baf027053e004
leastzero <csong7177@naver.com>
2026-01-15 16:27:57 +0900

    [feat] LikeButton, CommentSection 구현

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,48 +1,72 @@
-import type { Post } from '../types';
+export async function fetchPosts(page: number = 1, limit: number = 5) {
+    const response = await fetch(`api/posts?page=${page}&limit=${limit}`);
 
-export async function fetchPosts(page:number=1,limit:number=5){
-    const response=await fetch(`api/posts?page=${page}&limit=${limit}`);
-
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error('Faild to fetch posts');
     }
 
     return response.json();
 }
 
-export async function fetchPost(id:number){
-    const response=await fetch(`api/posts/${id}`);
+export async function fetchPost(id: number) {
+    const response = await fetch(`api/posts/${id}`);
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error('Failed to fetch post');
     }
 
     return response.json();
 }
 
-export async function createPost(content:string,image?:string){
-    const response=await fetch('api/posts',{
-        method:'POST',
-        headers:{
+export async function createPost(content: string, image?: string) {
+    const response = await fetch('api/posts', {
+        method: 'POST',
+        headers: {
             'Content-Type': 'application/json'
         },
-        body:JSON.stringify({content,image})
+        body: JSON.stringify({ content, image })
     });
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error('Failed to create post');
     }
 
     return response.json();
 }
 
-export async function likePost(postId:number){
-    const response=await fetch(`api/posts/${postId}/like`,{
-        method:'POST'
+export async function likePost(postId: number) {
+    const response = await fetch(`api/posts/${postId}/like`, {
+        method: 'POST'
     });
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error('Failed to like post');
+    }
+
+    return response.json();
+}
+
+export async function fetchComments(postId: number) {
+    const response = await fetch(`/api/posts/${postId}/comments`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch comments');
+    }
+
+    return response.json();
+}
+
+export async function createComment(postId: number, content: string) {
+    const response = await fetch('/api/comments', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ postId, content })
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to create comment');
     }
 
     return response.json();
