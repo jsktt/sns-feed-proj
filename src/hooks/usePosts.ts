3370dfea92e6cb602a8db3201597e195edd7e859
idayoung <dayounglee0311@gmail.com>
2026-01-15 16:30:38 +0900

    Feed

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchPost } from "../api/posts";
+import type { PostsResponse } from "../types";
 
 export function usePosts(page:number=1){
-    return useQuery(
+    return useQuery<PostsResponse>(
         {
             queryKey:['posts',page],
             queryFn:()=>fetchPost(page)
