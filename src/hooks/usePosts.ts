ad69ce2ca21b8fee160a080580ce1171410dfcc7
idayoung <dayounglee0311@gmail.com>
2026-01-15 16:02:51 +0900

    usePost.ts, PostCard.ts completed

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPost } from "../api/posts";
+
+export function usePosts(page:number=1){
+    return useQuery(
+        {
+            queryKey:['posts',page],
+            queryFn:()=>fetchPost(page)
+        }
+    );
+}
